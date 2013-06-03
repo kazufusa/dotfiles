285719391d18e1e60cb2c05ea697da6c546d0c0e
kazufusa <a.cluster.of.grapes@gmail.com>
2013-06-03 15:40:01 +0900

    Add vim utilities install in install_utils.sh.

--- scripts/install_utils.sh
+++ scripts/install_utils.sh
@@ -2,7 +2,7 @@
 sudo aptitude install -y ssh
 sudo aptitude install -y zsh
 sudo aptitude install -y autojump
-sudo aptitude install -y vim
+sudo aptitude install -y vim vim-gtk vim-athena vim-gnome
 #sudo aptitude install -y linux-headers-$(uname -r)
 sudo aptitude install -y build-essential
 sudo aptitude install -y zlib1g-dev
