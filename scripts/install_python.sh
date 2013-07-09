fef1dec5dbcab9adb0236d8fb5a718c310d40a76
kazufusa <a.cluster.of.grapes@gmail.com>
2013-07-10 05:46:28 +0900

    fix python setup

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sudo aptitude install -y python2.7 python2.7-dev python-virtualenv virtualenvwrapper
+sudo aptitude install -y python2.7 python2.7-dev python-virtualenv
+sudo easy_install virtualenvwrapper
