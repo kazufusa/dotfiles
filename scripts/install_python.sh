2595b0af6dae6c0f2441bc00d7a1597fe4667867
kazufusa <a.cluster.of.grapes@gmail.com>
2013-07-10 05:05:24 +0900

    modified python setup

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
-mkdir -p python_setup
-cd python_setup
-
-#install python-dev
-sudo aptitude install -y python-dev
-# install distribute.
-sudo chmod -R 0775 /usr/local
-sudo chgrp -R $USER /usr/local
-wget http://python-distribute.org/distribute_setup.py
-sudo python distribute_setup.py
-#install pip
-wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-sudo python get-pip.py
-#install virtualenv.
-pip install virtualenv
-pip install virtualenvwrapper
+aptitude install -y python2.7 python2.7-dev python-virtualenv virtualenvwrapper
