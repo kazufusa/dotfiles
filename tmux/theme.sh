11662d5863ee8d3542c79108d9fb66fa2a551cf8
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2014-07-03 15:04:02 +0900

    TMUX: added alarm

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -48,6 +48,7 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
 		#"battery 137 127" \
+		"alarm 89 255" \
 		"yf 37 255" \
 		"weather 37 255 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		#"rainbarf 0 0" \
