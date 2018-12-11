6bb1bdc77c56dcbbd30debbbc931b869db0cd34b
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2018-12-11 09:26:57 +0900

    Fix build parameters of tig

--- linux.sh
+++ linux.sh
@@ -25,4 +25,4 @@ sh -eux $DOTPATH/bin/build.sh vim/vim \
 "gettext libncurses5-dev libacl1-dev libgpm-dev libxmu-dev libgtk2.0-dev libxpm-dev libncurses5-dev libperl-dev python-dev python3-dev ruby-dev lua5.2 liblua5.2-dev exuberant-ctags" \
 " --with-features=huge --enable-gui=gtk2 --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-rubyinterp --enable-luainterp --enable-fail-if-missing --enable-multibyte"
 sh -eux $DOTPATH/bin/build.sh jonas/tig \
-"libncursesw5-dev" "--with-ncursesw LDFLAGS=-L/usr/local/ncursesw-5.9/lib CFLAGS=-I/usr/local/ncursesw-5.9/include"
+"libncursesw5-dev" "--enable-widec --with-ncursesw LDFLAGS=-L/usr/local/ncursesw-5.9/lib CFLAGS=-I/usr/local/ncursesw-5.9/include"
