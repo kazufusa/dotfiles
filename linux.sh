da6cde2f1b9a2e27c1caf72c92473b8573efb36f
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2018-12-11 10:32:30 +0900

    [VIM] Add RUNPATH to the lib path of pyenv
    ```
    $ readelf -d ~/bin/vim | grep pyenv
     0x000000000000001d (RUNPATH)            Library runpath: [/home/xxxxx/.anyenv/envs/pyenv/versions/3.6.6/lib]
    ```

--- linux.sh
+++ linux.sh
@@ -21,6 +21,7 @@ sudo apt-get install -y w3m
 sudo chsh -s `which zsh` $USER
 sh -eux $DOTPATH/bin/build.sh tmux/tmux \
 "pkg-config libevent-dev libncurses5-dev xsel automake" ""
+LDFLAGS="-Wl,-rpath=$(pyenv root)/versions/$(pyenv global)/lib " \
 sh -eux $DOTPATH/bin/build.sh vim/vim \
 "gettext libncurses5-dev libacl1-dev libgpm-dev libxmu-dev libgtk2.0-dev libxpm-dev libncurses5-dev libperl-dev python-dev python3-dev ruby-dev lua5.2 liblua5.2-dev exuberant-ctags" \
 " --with-features=huge --enable-gui=gtk2 --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-rubyinterp --enable-luainterp --enable-fail-if-missing --enable-multibyte"
