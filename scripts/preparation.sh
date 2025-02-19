a473a0bbfa409435fc6a68f215634dd0f9d25f52
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2025-02-19 10:04:17 +0900

    update new

--- scripts/preparation.sh
+++ scripts/preparation.sh
@@ -5,6 +5,7 @@ set -Ceux
 if [ "$(uname)" == 'Linux' ]; then
   apt-get update
   apt-get upgrade -y
-  apt-get install -y curl make build-essential git zsh
+  apt-get install -y curl make build-essential git zsh locales
   chsh -s /bin/zsh
+  locale-gen en_US.utf-8
 fi
