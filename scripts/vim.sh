eb1d1aca89a44a4d95b698e3d707cd6827e33b8e
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2025-03-02 22:26:47 +0900

    update new

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -7,10 +7,7 @@ ismac && exit
 export PATH=$(echo $PATH | sed -r 's/\/home\/linuxbrew[^:]*://g')
 
 sudo apt-get update -qq
-sudo apt-get install -y gettext libncurses5-dev libacl1-dev libgpm-dev libxmu-dev libgtk2.0-dev libxpm-dev libncurses5-dev libperl-dev python-dev python3-dev ruby-dev lua5.2 liblua5.2-dev exuberant-ctags
-
-sudo apt-get install -y python3-pip
-# sudo python3 -m pip install pynvim
+sudo apt-get install -y gettext libncurses5-dev libacl1-dev libgpm-dev libxmu-dev libgtk2.0-dev libxpm-dev libncurses5-dev libperl-dev  python3-dev ruby-dev lua5.2 liblua5.2-dev exuberant-ctags
 
 if [ ! -d $HOME/src/github.com/vim/vim ]; then
   git clone https://github.com/vim/vim $HOME/src/github.com/vim/vim
